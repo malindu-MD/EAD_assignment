e9ea3ed5cb7456e1bd750962b64d37f4a1fc62d8
Malindu Sooriyaarachchi <malindusooriyaarachchi@gmail.com>
2024-10-17 15:53:01 +0530

    update queryparameter

--- ecommerceWebServicess/ecommerceWebServicess/DTOs/ProductQueryParameters.cs
+++ ecommerceWebServicess/ecommerceWebServicess/DTOs/ProductQueryParameters.cs
@@ -2,20 +2,20 @@
 {
     public class ProductQueryParameters
     {
-        public string Name { get; set; } // Search by product name
+        public string? Name { get; set; } // Search by product name
 
-        public string CategoryId { get; set; } // Filter by category ID
+        public string? CategoryId { get; set; } // Filter by category ID
 
         public decimal? MinPrice { get; set; } // Minimum price filter
 
         public decimal? MaxPrice { get; set; } // Maximum price filter
 
-        public string VendorId { get; set; } // Filter by vendor ID
+        public string? VendorId { get; set; } // Filter by vendor ID
 
         public bool? IsActive { get; set; } // Filter by active/inactive status
 
-        public int PageNumber { get; set; } = 1; // The page number to display (for pagination)
+        public int? PageNumber { get; set; } = 1; // The page number to display (for pagination)
 
-        public int PageSize { get; set; } = 10; // The number of items to return per page (for pagination)
+        public int? PageSize { get; set; } = 10; // The number of items to return per page (for pagination)
     }
 }
